78cb6f323c82b625a414f98e0f2e24e1cfd4eb7b
Joel Flores <j_flores88@outlook.com>
2020-09-14 20:54:00 -0700

    Fixed broken code for ftoc in tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  if ((far - 32) * 5/9 === 0) {
+    return 0
+  } else {
+    return Number(((far - 32) * 5/9).toFixed(1))
+  }
 }
-
 const ctof = function() {
 
 }
