fe6d93810f6c8d52ef3c2a4f6cc9b7d054507760
Joel Flores <j_flores88@outlook.com>
2020-09-14 18:54:16 -0700

    Final working code for removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
-}
+const removeFromArray = function(arr, ...arguments) {
+    let args = [...arguments]
+  
+    for(var i = 0; i < args.length; i++) {
+      input = args[i]
+      for(var j = 0; j < arr.length; j++) {
+        if(input === arr[j]) {
+          arr.splice(j, 1)
+        }
+      }
+    }
+    return arr
+  }
 
 module.exports = removeFromArray
