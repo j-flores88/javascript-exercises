d35e947af6b4ef1a993d889c4774fa4bbe83a59e
Joel Flores <j_flores88@outlook.com>
2020-09-14 17:49:27 -0700

    Final working code for repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
-}
+const repeatString = function(str, num) {
+    result = ''
+    const number = Math.floor(Math.random() * 1000)
+  
+    if(num === undefined) {
+      for (var i = 0; i < number; i++) {
+      result += str;
+      }
+    } else if (num < 0) {
+      return 'ERROR'
+    } else {
+      for (var i = 0; i < num; i++) {
+        result += str
+      }
+    }
+    return result
+  }
 
 module.exports = repeatString
