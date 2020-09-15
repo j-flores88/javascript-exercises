e5de2cece6c022d9309f755be825eb212a20d937
Joel Flores <j_flores88@outlook.com>
2020-09-14 18:09:45 -0700

    Final working code for reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const result = ''
+  
+    const strArr = str.split('')
+    const newArr = []
+  
+    for(var i = strArr.length - 1; i >= 0; i--) {
+      newArr.push(strArr[i])
+    }
+    return newArr.join('')
 }
 
 module.exports = reverseString
