643a50fb3aa433988e47b0d777bde205acd85ba5
Joel Flores <j_flores88@outlook.com>
2020-12-14 21:45:20 -0800

    Final working code for palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function() {
-
-}
+const palindromes = function(str) {
+    str = str.replace(/\s+/g, '');
+    let arr = str.split('')
+    let rev = []
+  
+      for (let i = 0; i < arr.length; i++) {
+       if (arr[i].toUpperCase() === arr[i].toLowerCase()) {
+         arr.splice(i, 1)
+       } else {
+         arr[i] = arr[i].toLowerCase();
+       }
+      }
+      for(j = arr.length - 1; j >= 0; j--) {
+        rev.push(arr[j])
+      }
+      for (let i = 0; i < arr.length; i++) {
+       if (arr[i] != rev[i]) {
+         return false;
+       }
+      }
+    return true
+  }
 
 module.exports = palindromes
