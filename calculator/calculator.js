57ca44aaa68941e29e9a773b1bcea93714328012
Joel Flores <j_flores88@outlook.com>
2020-12-14 19:41:02 -0800

    All calculator tests passing

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,54 @@
-function add () {
-	
+function add (a, b) {
+	return (a + b);
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return (a - b);
 }
 
-function sum () {
-	
+function sum (input) {
+	let sum = 0;
+
+	if(!Array.isArray(input)) {
+		return 'Error'
+	} else if(input.length === 0) {
+		return 0
+	} else {
+		for(let i = 0; i < input.length; i++) {
+      sum += input[i]
+		}
+	}
+  return sum
 }
 
-function multiply () {
-	
+function multiply (input) {
+	let result = 1;
+
+	if(!Array.isArray(input)) {
+		return 'Error'
+	} else if(input.length === 0) {
+		return 0
+	} else {
+		for(let i = 0; i < input.length; i++) {
+      result *= input[i]
+		}
+	}
+  return result
 }
 
-function power() {
-	
+function power(a, b) {
+	return (a**b)
 }
 
-function factorial() {
-	
-}
+function factorial(input) {
+	let result = 1;
+  
+	  while(input > 0) {
+	  result *= input
+	  input--
+	}
+	return result
+  }
 
 module.exports = {
 	add,
