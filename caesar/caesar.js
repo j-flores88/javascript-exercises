bc1059f159b054099aea5e2b5ac4e83ec9c3edb4
Joel Flores <j_flores88@outlook.com>
2020-12-15 21:37:58 -0800

    All tests for caesar cipher passing.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,18 @@
-const caesar = function() {
-
-}
-
+const caesar = function(str, shift) {
+    let result = []
+  
+    for(i = 0; i < str.length; i++) {
+      if(/[a-zA-z]/.test(str[i])) {
+        const start = str[i] === str[i].toLowerCase() ? 'a'.charCodeAt(0) : 'A'.charCodeAt(0);
+        const diff = str[i].charCodeAt(0) - start;
+        const move = shift >= 0 ? diff + shift : diff + Math.abs(26 - Math.abs(shift));
+        const code = move % 26 + start
+        result.push(String.fromCharCode(code))
+        //console.log(str[i].charCodeAt(0), start, diff, move, code)
+      } else {
+        result.push(str[i])
+      }
+    }
+    return result.join('');
+  }
 module.exports = caesar
