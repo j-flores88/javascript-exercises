c67404600af4e1e390afb6db0fc9998395ee3fd2
Joel Flores <j_flores88@outlook.com>
2020-12-17 16:56:45 -0800

    Test passing for getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function(bookList) {
+    const result = [];
+  
+    for(let i = 0; i < bookList.length; i++) {
+      let currentBook = bookList[i]
+      for(let key in currentBook)
+        //console.log(currentBook[key], key)
+        if(key === 'title') {
+          result.push(currentBook[key])
+        }
+    }
+    return result
+  }
 
 module.exports = getTheTitles;
