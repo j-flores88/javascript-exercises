4dd1635b4fd4b5586339ae2b180556f9505b4bf4
Joel Flores <j_flores88@outlook.com>
2020-12-17 16:31:39 -0800

    All tests passing for fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
-
-}
+const fibonacci = function(num) {
+    a = 0;
+    b= 1;
+    c = 1;
+  
+    if(typeof(num) === 'string') {
+      num = parseInt(num)
+    }
+  
+    if(num === 0) {
+      return 0;
+    } else if (num < 0) {
+      return "OOPS"
+    } else {
+      for(let i = 2; i <= num; i++) {
+        c = a + b;
+  
+        a = b
+        b = c
+        //console.log(a, b, c)
+      }
+        
+    }
+  return c
+  }
 
 module.exports = fibonacci
