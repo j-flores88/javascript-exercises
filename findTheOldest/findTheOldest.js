5138db836d13a505da0679bfd84aaf874ee61b07
Joel Flores <j_flores88@outlook.com>
2020-12-25 13:13:28 -0800

    All tests passing for findTheOldest.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
-
-}
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, person) => {
+  
+      if(person.yearOfDeath === undefined) {
+        person.yearOfDeath = new Date().getFullYear()
+      }
+      if(oldest.yearOfDeath === undefined) {
+        oldest.yearOfDeath = new Date().getFullYear()
+      }
+  
+      if ((person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest. yearOfBirth)) {
+        return person
+      } else if ((person.yearOfDeath - person.yearOfBirth) <= (oldest.yearOfDeath - oldest. yearOfBirth)) {
+        return oldest
+      }
+    })
+    return oldestPerson 
+  }
 
 module.exports = findTheOldest
